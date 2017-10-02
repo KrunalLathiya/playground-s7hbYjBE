590dc485d8f383e696d9e01dc56a99d90a2961df
KrunalLathiya <krunallathiya21@gmail.com>
2017-10-02 15:19:09 +0530

    complete react-router tutorial

---
+++ react-project/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+class App extends Component {
+  render() {
+    return (
+    <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+            <li><Link to={'/about'} className="nav-link">About</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/about' component={About} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
